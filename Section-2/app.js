4e87efea8a850b16a531fa639e6a03783958bcdf
MANISH-GIRI <manish.giri.me@gmail.com>
2017-03-22 04:04:59 -0400

    Pass arguments to functions, use mousemove event with v-on directive

--- Section-2/app.js
+++ Section-2/app.js
@@ -1,4 +1,4 @@
-new new Vue({
+new Vue({
   el: "#app",
   data: {
     title: "Hello World!",
@@ -6,12 +6,30 @@ new new Vue({
     finishedLink: '<a href="http://google.com" target="_blank">Google</a>'
   },
   methods: {
-    changeTitle: function(e) {
-      this.title = e.target.value;
-    },
-    sayHello: function() {
-      this.title = "Hello!";
-      return this.title;
+    changeTitle: function(event) {
+      this.title = event.target.value;
     }
+    // sayHello: function() {
+    //   this.title = "Hello!";
+    //   return this.title;
+    // }
   }
-})
+});
+
+new Vue({
+    el: "#event",
+    data: {
+        counter: 0,
+        x: 0,
+        y: 0
+    },
+    methods: {
+        increment: function (step) {
+            this.counter += step;
+        },
+        updateCoordinates: function (event) {
+            this.x = event.clientX;
+            this.y = event.clientY;
+        }
+    }
+});
